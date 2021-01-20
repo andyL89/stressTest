65f75d6d45c7d29c89eaf1e505e967eaabeb7e15
Andy Lawson <alawson89@gmail.com>
2021-01-20 11:51:31 -0800

    create project, html, and js logic
    Co-authored-by: Daniel Adeyami<adeyamidanny@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
-
-
-
+  $("#stress").submit(function(event) {
+    event.preventDefault();
+    let symptomArray = [];
+    let warningArray = [];
+    let suggestion;
+    $("input:checkbox[name=stress-survey]:checked").each(function() {
+      let i = $(this).val();
+      symptomArray.push(i);
+    });
+    $("input:checkbox[name=warningSign]:checked").each(function() {
+      let i = $(this).val();
+      warningArray.push(i);
+    });
+    if (symptomArray.length > 2) {
+      for (let i = 0; i < warningArray.length; i++) {
+        if (warningArray[i] === "happy" || warningArray[i] === "relaxed") {
+          suggestion = "Try to meditate more";
+        } else {
+          suggestion = "Probably you need more rest!";
+        }
+      }
+    } else {
+      suggestion = "Maybe you can go for a walk";
+    }
+    $(".output").text(suggestion);
+  });
 });
